9f56bbe60c9778c31f379bbae23fc986fec4ac6d
Kairung Vangmanaw <94585323+VangmanawKairung@users.noreply.github.com>
2026-09-25 18:24:39 +0700

    refactor: update Preparation.jsx

--- src/components/PreparationTime.jsx
+++ src/components/PreparationTime.jsx
@@ -1,23 +1,25 @@
-const PreparationTime = () => {
-  const PREPARATION_DATA = [
-    { topic: "Total", time: 10, approximate: true },
-    { topic: "Preparation", time: 5, approximate: false },
-    { topic: "Cooking", time: 5, approximate: false },
-  ];
+const PREPARATION_DATA = [
+  { topic: "Total", time: 10, approximate: true },
+  { topic: "Preparation", time: 5, approximate: false },
+  { topic: "Cooking", time: 5, approximate: false },
+];
 
-  return (
-    <article className="prep-time">
-      <h3 className="prep-time__title">Preparation time</h3>
-      <ul className="prep-time__list">
-        {PREPARATION_DATA.map((item) => (
-          <li className="prep-time__item" key={item.topic}>
-            <em className="prep-time__item-emphasize">{item.topic}:</em>{" "}
+const PreparationTime = () => (
+  <aside className="prep-time" aria-label="Preparation time summary">
+    <h2 className="prep-time__title">Preparation time</h2>
+    <ul className="prep-time__list">
+      {PREPARATION_DATA.map((item) => (
+        <li className="prep-time__item" key={item.topic}>
+          <span className="prep-time__content">
+            <strong className="prep-time__label">
+              {item.topic}:
+            </strong>{" "}
             {item.approximate ? "Approximately" : ""} {item.time} minutes
-          </li>
-        ))}
-      </ul>
-    </article>
-  );
-};
+          </span>
+        </li>
+      ))}
+    </ul>
+  </aside>
+);
 
 export default PreparationTime;
