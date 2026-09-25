9f892df51b3a8085d504e500ec5e22ea1dbc8a38
Kairung Vangmanaw <94585323+VangmanawKairung@users.noreply.github.com>
2026-09-25 18:27:12 +0700

    refactor: update Ingredients.jsx

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -4,17 +4,19 @@ const INGREDIENTS_DATA = [
   "Pepper, to taste",
   "1 tablespoon of butter or oil",
   "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
-]
+];
 
 const Ingredients = () => (
   <section className="ingredients">
     <h2 className="ingredients__title">Ingredients</h2>
     <ul className="ingredients__list">
-        {INGREDIENTS_DATA.map((item, index) => (
-            <li className="ingredients__item" key={`ingredient-${index+1}`}>{item}</li>
-        ))}
+      {INGREDIENTS_DATA.map((item, index) => (
+        <li className="ingredients__item" key={`ingredient-${index + 1}`}>
+          <span className="ingredients__content">{item}</span>
+        </li>
+      ))}
     </ul>
   </section>
 );
 
-export default Ingredients
+export default Ingredients;
